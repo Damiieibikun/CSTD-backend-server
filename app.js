ba3da82f692946c1e93d934714c0fc4253fd37b5
Damilola Ibikunle <damiieibikun@gmail.com>
2025-08-25 13:50:48 +0100

    finalised hosting on render

--- app.js
+++ app.js
@@ -4,14 +4,13 @@ const app = express();
 const {config} = require('./config/config')
 const cors = require('cors');
 const path = require('path');
-app.use(cors());
-// app.use(cors({
-//     origin: 'https://hive-clone.onrender.com', 
-//     methods: 'GET,POST,PUT,DELETE',
-//     allowedHeaders: 'Content-Type,Authorization'
-//   }));
+app.use(cors({
+    origin: 'https://cstd-backend-server.onrender.com', 
+    methods: 'GET,POST,PUT,DELETE',
+    allowedHeaders: 'Content-Type,Authorization'
+  }));
   
-// app.options('*', cors());
+app.options('*', cors());
 app.use(express.json({ limit: '1mb' }));
 app.use(express.urlencoded({ limit: '1mb', extended: true }));
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')))
