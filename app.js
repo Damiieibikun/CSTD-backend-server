0a812471cfe0dd90e128d1e9025cdb5afeab5414
Damilola Ibikunle <damiieibikun@gmail.com>
2025-08-25 13:35:19 +0100

    .

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.use(cors());
 //     allowedHeaders: 'Content-Type,Authorization'
 //   }));
   
-    app.options('*', cors());
+// app.options('*', cors());
 app.use(express.json({ limit: '1mb' }));
 app.use(express.urlencoded({ limit: '1mb', extended: true }));
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')))
