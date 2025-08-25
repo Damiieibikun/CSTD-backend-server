1a4c9e2a9ced93b3292072cff3c9f5982f5015f6
Damilola Ibikunle <damiieibikun@gmail.com>
2025-08-25 13:26:43 +0100

    added config for hosting

--- app.js
+++ app.js
@@ -5,6 +5,13 @@ const {config} = require('./config/config')
 const cors = require('cors');
 const path = require('path');
 app.use(cors());
+// app.use(cors({
+//     origin: 'https://hive-clone.onrender.com', 
+//     methods: 'GET,POST,PUT,DELETE',
+//     allowedHeaders: 'Content-Type,Authorization'
+//   }));
+  
+    app.options('*', cors());
 app.use(express.json({ limit: '1mb' }));
 app.use(express.urlencoded({ limit: '1mb', extended: true }));
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')))
