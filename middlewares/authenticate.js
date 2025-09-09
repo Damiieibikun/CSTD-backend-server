3b497eaa1f98cb427743160bbdbf571542eaa320
Ddilibe <franklinfidelugwuowo@gmail.com>
2025-09-09 10:52:33 +0100

    [Chore]: Created middleware for the project

--- middlewares/authenticate.js
+++ middlewares/authenticate.js
@@ -1,12 +1,9 @@
 const jwt = require("jsonwebtoken");
 
 const JWTAuthenticate = async (req, res, next) => {
-    const authHeader = req.headers.authorization;
-    if (!authHeader || !authHeader.startswith("Bearer")) {
-        res.status(401).json({ message: "No token Provided" });
-    }
+    const token = req.cookies.authToken;
 
-    const token = authHeader.split(" ")[1];
+    if (!token) return res.status(401).json({ message: 'No token provided' });
 
     try {
         const user = jwt.verify(token, process.env.JWT_TOKEN);
